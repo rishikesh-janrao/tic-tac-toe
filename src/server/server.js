c42ed14b12bdee09a470d0fedf4e41120c3ceefb
Rishikesh Janrao <65944832+rishikesh-janrao@users.noreply.github.com>
2024-04-30 23:02:37 +0530

    [Architecture] Adds local environment variable implementation

--- src/server/server.js
+++ src/server/server.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const cors = require("cors");
 const { readFileSync } = require("fs");
+const { config } = require("dotenv");
+//use .env file for process level config
+config({
+  path: "../.env",
+});
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+const PORT = process.env.SERVER_PORT || 3001;
 
 app.get("/translations", (req, res) => {
   const controlTranslations = JSON.parse(
@@ -16,5 +23,6 @@ app.get("/translations", (req, res) => {
     .status(200)
     .end(JSON.stringify({ ...controlTranslations, ...infoTranslations }));
 });
-
-app.listen(3001);
+app.listen(PORT, () => {
+  console.log(`Started server at http://localhost:${PORT}.`);
+});
